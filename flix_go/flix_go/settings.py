f009f96dd75bfc9da8904e3549b320db3d3f1420
Bharat Pant <bharatpant1091@Bharats-MacBook-Pro.local>
2023-01-10 15:47:31 +0000

    third change of host

--- flix_go/flix_go/settings.py
+++ flix_go/flix_go/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-m&qgierwy$qj@=%h5__6fnr^bac#bvxyg30u@k=yb%r0p8^ihw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
