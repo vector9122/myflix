e2856718c1abffef410160b82d3ab81f29229e82
Bharat Pant <bharatpant1091@Bharats-MacBook-Pro.local>
2023-01-10 14:43:44 +0000

    changed the allowed host setting

--- flix_go/flix_go/settings.py
+++ flix_go/flix_go/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-m&qgierwy$qj@=%h5__6fnr^bac#bvxyg30u@k=yb%r0p8^ihw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
